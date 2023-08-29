65a31b6b3f0ca0ae6667f0e24b61b591ef25249f
Basab Ghosh <cs21b1068@iiitdm.ac.in>
2023-08-29 23:39:09 +0530

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+#Basab Ghosh cs21b1068
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
